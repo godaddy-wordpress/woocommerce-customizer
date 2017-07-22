6c00ee951bd155e8a67f9b4efa0a573a3619752a
Beka Rice <beka.rice@gmail.com>
2017-07-22 12:24:01 -0400

    Update WC outdated notice to prompt for db updater

--- woocommerce-customizer.php
+++ woocommerce-customizer.php
@@ -25,54 +25,14 @@ defined( 'ABSPATH' ) or exit;
 
 // Check if WooCommerce is active
 if ( ! WC_Customizer::is_woocommerce_active() ) {
-	add_action( 'admin_notices', 'wc_customizer_render_wc_inactive_notice' );
+	add_action( 'admin_notices', array( 'WC_Customizer', 'render_wc_inactive_notice' ) );
 	return;
 }
 
 // WC version check
 if ( version_compare( get_option( 'woocommerce_db_version' ), '2.5.5', '<' ) ) {
-	add_action( 'admin_notices', 'wc_customizer_render_outdated_wc_version_notice' );
+	add_action( 'admin_notices', array( 'WC_Customizer', 'render_outdated_wc_version_notice' ) );
 	return;
-}
-
-
-/**
- * Renders a notice when WooCommerce version is outdated
- *
- * @since 2.3.1
- */
-function wc_customizer_render_outdated_wc_version_notice() {
-
-	$message = sprintf(
-		/* translators: %1$s and %2$s are <strong> tags. %3$s and %4$s are <a> tags */
-		__( '%1$sWooCommerce Customizer is inactive.%2$s This version requires WooCommerce 2.5.5 or newer. Please %3$supdate WooCommerce to version 2.5.5 or newer%4$s', 'woocommerce-customizer' ),
-		'<strong>',
-		'</strong>',
-		'<a href="' . admin_url( 'plugins.php' ) . '">',
-		'&nbsp;&raquo;</a>'
-	);
-
-	printf( '<div class="error"><p>%s</p></div>', $message );
-}
-
-
-/**
- * Renders a notice when WooCommerce version is outdated
- *
- * @since 2.3.1
- */
-function wc_customizer_render_wc_inactive_notice() {
-
-	$message = sprintf(
-		/* translators: %1$s and %2$s are <strong> tags. %3$s and %4$s are <a> tags */
-		__( '%1$sWooCommerce Customizer is inactive%2$s as it requires WooCommerce. Please %3$sactivate WooCommerce version 2.5.5 or newer%4$s', 'woocommerce-customizer' ),
-		'<strong>',
-		'</strong>',
-		'<a href="' . admin_url( 'plugins.php' ) . '">',
-		'&nbsp;&raquo;</a>'
-	);
-
-	printf( '<div class="error"><p>%s</p></div>', $message );
 }
 
 
@@ -262,6 +222,47 @@ class WC_Customizer {
 		}
 
 		return in_array( 'woocommerce/woocommerce.php', $active_plugins ) || array_key_exists( 'woocommerce/woocommerce.php', $active_plugins );
+	}
+
+
+	/**
+	 * Renders a notice when WooCommerce version is outdated
+	 *
+	 * @since 2.3.1
+	 */
+	public static function render_wc_inactive_notice() {
+
+		$message = sprintf(
+		/* translators: %1$s and %2$s are <strong> tags. %3$s and %4$s are <a> tags */
+			__( '%1$sWooCommerce Customizer is inactive%2$s as it requires WooCommerce. Please %3$sactivate WooCommerce version 2.5.5 or newer%4$s', 'woocommerce-customizer' ),
+			'<strong>',
+			'</strong>',
+			'<a href="' . admin_url( 'plugins.php' ) . '">',
+			'&nbsp;&raquo;</a>'
+		);
+
+		printf( '<div class="error"><p>%s</p></div>', $message );
+	}
+
+
+	/**
+	 * Renders a notice when WooCommerce version is outdated
+	 *
+	 * @since 2.3.1
+	 */
+	public static function render_outdated_wc_version_notice() {
+
+		$message = sprintf(
+		/* translators: Placeholders: %1$s <strong>, %2$s - </strong>, %3$s and %5$s - <a> tags, %4$s - </a> */
+			__( '%1$sWooCommerce Customizer is inactive.%2$s This plugin requires WooCommerce 2.5.5 or newer. Please %3$supdate WooCommerce%4$s or %5$srun the WooCommerce database upgrade%4$s.', 'woocommerce-customizer' ),
+			'<strong>',
+			'</strong>',
+			'<a href="' . admin_url( 'plugins.php' ) . '">',
+			'</a>',
+			'<a href="' . admin_url( 'plugins.php?do_update_woocommerce=true' ) . '">'
+		);
+
+		printf( '<div class="error"><p>%s</p></div>', $message );
 	}
 
 
