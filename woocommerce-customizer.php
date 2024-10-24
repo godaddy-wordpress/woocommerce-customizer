a7b1d723855ff2ba7e60d49bef3e7bf793290276
Andrew Jaynes <ajaynes@godaddy.com>
2024-10-23 21:23:28 -0600

    Bump the version, stable tag to 2.9.0 and add a changelog.

--- woocommerce-customizer.php
+++ woocommerce-customizer.php
@@ -5,7 +5,7 @@
  * Description: Customize WooCommerce without code! Easily change add to cart button text and more.
  * Author: SkyVerge
  * Author URI: http://www.skyverge.com
- * Version: 2.8.0
+ * Version: 2.9.0-dev.1
  * Text Domain: woocommerce-customizer
  * Domain Path: /i18n/languages/
  *
@@ -46,7 +46,7 @@ class WC_Customizer {
 
 
 	/** plugin version number */
-	const VERSION = '2.8.0';
+	const VERSION = '2.9.0-dev.1';
 
 	/** required WooCommerce version number */
 	const MIN_WOOCOMMERCE_VERSION = '3.9.4';
